e0e1c53a0887b418ff428392d10b6ec52dded5f0
dylan <dylangthomas1@gmail.com>
2023-08-17 20:48:19 -0600

    only getting started

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     const { method } = req
 
     switch(method) {
         case 'GET':
-            getHandler()
+            getHandler(req, res)
+            break;
     }
 })
 
-server.listen(3000, () => {
+server.listen(3000, 'localhost', () => {
 
 })
 
 const getHandler = (req, res) => {
     const pathname = req.url
+    let path = './pages/'
     console.log(pathname)
+    switch(pathname) {
+        case '/':
+            path += 'index.html'
+            break;
+        case '/about':
+            path += 'about.html'
+            break;
+        case '/contact':
+            path += 'contact.html'
+            break;
+        default:
+            path += '404.html'
+    }
+
+    fs.readFile(path, (err, data) => {
+        if(err) {
+            console.log(err)
+            res.end()
+        } else {
+            res.write(data)
+            res.end()
+        }
+    })
 }
